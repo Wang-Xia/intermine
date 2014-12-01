380bc76683097f1220cf63488fa30e1b3c1ae679
julie-sullivan <julie@flymine.org>
2014-12-01 12:26:10 +0000

    add human to entrez gene resolver test

--- bio/core/test/src/org/intermine/bio/dataconversion/EntrezGeneIdResolverFactoryTest.java
+++ bio/core/test/src/org/intermine/bio/dataconversion/EntrezGeneIdResolverFactoryTest.java
@@ -46,7 +46,7 @@ public class EntrezGeneIdResolverFactoryTest extends TestCase {
         assertEquals(6, factory.getXrefs().size());
         assertTrue(factory.getXrefs().containsKey("7955"));
         assertFalse(factory.getXrefs().containsValue("OMIM"));
-        assertTrue(factory.getPrefixes().containsKey("10090"));
+        assertTrue(factory.getPrefixes().containsKey("10116"));
         assertNotNull(factory.getStrain(Collections.singleton("559292")));
         assertTrue(factory.getIgnoredTaxonIds().contains("6239"));
 
@@ -84,10 +84,13 @@ public class EntrezGeneIdResolverFactoryTest extends TestCase {
             fail("data file not found");
         }
 
-        factory.createFromFile(entrezFile, new HashSet<String>(Arrays.asList(new String[] {"7227", "4932", "10090", "7955"})));
-        assertTrue(IdResolverFactory.resolver.getTaxons().size() == 5);
-        assertEquals(new LinkedHashSet<String>(Arrays.asList(new String[] {"7955", "102", "4932", "101", "10090"})), IdResolverFactory.resolver.getTaxons());
+        factory.createFromFile(entrezFile, new HashSet<String>(Arrays.asList(new String[] {"7227", "4932", "10090", "7955", "9606"})));
+        assertTrue(IdResolverFactory.resolver.getTaxons().size() == 6);
+        assertEquals(new LinkedHashSet<String>(Arrays.asList(new String[] {"7955", "102", "4932", "101", "10090", "9606"})), IdResolverFactory.resolver.getTaxons());
         String mouseGene = IdResolverFactory.resolver.resolveId("10090", "gene", "Abca2").iterator().next();
         assertTrue("MGI:99606".equals(mouseGene));
+
+        String humanGene = IdResolverFactory.resolver.resolveId("9606", "gene", "ENSG00000121410").iterator().next();
+        assertTrue("1".equals(humanGene));
     }
 }
